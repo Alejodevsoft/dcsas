b465adefd3c5f2d4d4a189da7213ef17ca9c85cd
Alejodevsoft <alejodevsoft@gmail.com>
2022-11-25 11:47:19 -0500

    Recibe el parametro

--- src/main/java/com/dcsas/saludo/controllers/Saludo.java
+++ src/main/java/com/dcsas/saludo/controllers/Saludo.java
@@ -3,9 +3,11 @@ package com.dcsas.saludo.controllers;
 import com.dcsas.saludo.services.SaludoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/saludo")
@@ -14,7 +16,7 @@ public class Saludo {
     SaludoService saludoService;
 
     @PostMapping()
-    public String saludar(@RequestParam String name){
-        return saludoService.generarSaludo(name);
+    public String saludar(@RequestBody Map<String,String> params){
+        return saludoService.generarSaludo(params.get("name"));
     }
 }
